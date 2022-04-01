58d2cfe29cffe8824a9f543e8e1635b0012f50d1
finleykim <g_kimmm@naver.com>
2022-04-01 18:45:40 +0900

    적용되는 경우, 이 커밋은 타이머를 설정할 것이다.

--- pomodoro/ViewController.swift
+++ pomodoro/ViewController.swift
@@ -15,10 +15,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var toggleButton: UIButton!
     
-
+    
     var duration = 60
     var timerStatus: TimerStatus = .end
-    
+    var timer: DispatchSourceTimer?
+    //현재 카운트다운되고있는 시간을 초로 저장하는 프로퍼티
+    var currentSeconds = 0
 
     
     override func viewDidLoad() {
@@ -36,42 +38,68 @@ class ViewController: UIViewController {
         self.toggleButton.setTitle("일시정지", for: .selected)
     }
     
+
+    func startTimer(){
+            if self.timer == nil{
+                self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+                self.timer?.schedule(deadline: .now(), repeating: 1)
+                self.timer?.setEventHandler(handler: {[weak self] in self?.currentSeconds -= 1
+                                if self?.currentSeconds ?? 0 <= 0{
+                                    debugPrint(self?.currentSeconds)
+                                    self?.stopTimer()
+
+    }
+   })
+                self.timer?.resume()
+                                          
+            }
+        }
     
-    @IBAction func tapCancelButton(_ sender: Any) {
+    
+    func stopTimer(){
+        if self.timerStatus == .pause{
+            self.timer?.resume()
+        }
         self.timerStatus = .end
         self.setTimerInfoViewVisble(isHidden: true)
         self.datePicker.isHidden = false
         self.toggleButton.isSelected = false
         self.cancelButton.isEnabled = false
+        self.timer?.cancel()
+        //타이머를 종료할 때 nil을 할당하여 메모리에서 해제시켜야한다. 그렇지않으면 화면이 벗어나도 타이머가 계속동작됨
+        self.timer = nil
+    }
+    
+    
+    @IBAction func tapCancelButton(_ sender: Any) {
+
+        self.stopTimer()
         
     }
+  
     @IBAction func tapToggleButton(_ sender: Any) {
-        self.duration = Int(self.datePicker.countDownDuration)
-        switch self.timerStatus{
-        case .end: //클릭 -> 시작
-            self.timerStatus = .start
-            //1. 타이머와 프로그레스뷰를 숨기지 말고
-            self.setTimerInfoViewVisble(isHidden: false)
-            //2. 데이트피커는 숨기고
-            self.datePicker.isHidden = true
-            //3. 버튼은 선택된상태이므로 "일시정지"로변경 (configuareToggleButton메서드의 .selected)
-            self.toggleButton.isSelected = true
-            //4. 취소버튼은 활성화
-            self.cancelButton.isEnabled = true
-            
-        case .start: //클릭 -> 일시정지
-            //버튼을 누르면 .pause 상태로 변경
-            self.timerStatus = .pause
-            //1. 버튼은 선택된상태이므로 "시작"으로 변경 (configuareToggleButton메서드의 .selected가 아님을 표시 즉 .normal인 "시작")
-            self.toggleButton.isSelected = false
         
+        self.duration = Int(self.datePicker.countDownDuration)
+                switch self.timerStatus{
+                case .end:
+                    self.currentSeconds = self.duration
+                    self.timerStatus = .start
+                    self.setTimerInfoViewVisble(isHidden: false)
+                    self.datePicker.isHidden = true
+                    self.toggleButton.isSelected = true
+                    self.cancelButton.isEnabled = true
+                    self.startTimer()
+                case .start:
+                    self.timerStatus = .pause
+                    self.toggleButton.isSelected = false
+                    self.timer?.suspend()
+                case .pause:
+                    self.timerStatus = .start
+                    self.toggleButton.isSelected = true
+                    self.timer?.resume()
+             
+                }
             
-        case .pause: //클릭 -> 시작
-            self.timerStatus = .start
-            self.toggleButton.isSelected = true
-     
-        }
-    
         
     }
     
