7f9cc033ee8709e3019881be9479b24212fb5d1f
finleykim <g_kimmm@naver.com>
2022-04-05 15:23:56 +0900

    Initial Commit

---
+++ COVID19/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by Finley on 2022/04/05.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var totalCaseLabel: UIStackView!
+    @IBOutlet weak var newCaseLabel: UIStackView!
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
