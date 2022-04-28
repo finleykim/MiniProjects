de5de65e5284522781ec3e1f9ffb96b25d5facbc
finleykim <g_kimmm@naver.com>
2022-04-28 10:29:10 +0900

    add GridVIew

---
+++ MyAssets/MyAssets/AssetView.swift
@@ -0,0 +1,32 @@
+//
+//  AssetView.swift
+//  MyAssets
+//
+//  Created by Finley on 2022/04/28.
+//
+
+import SwiftUI
+
+struct AssetView: View {
+    var body: some View {
+        NavigationView{
+            //scrollview생성
+            ScrollView {
+                VStack(spacing: 30){
+                    Spacer()
+                    AssetMenuGridView()
+                }
+            }
+            
+            .background(Color.gray.opacity(0.2))
+            //상단 네비게이션 불러오기
+            .navigationBarwithButton("내 자산")
+        }
+    }
+}
+
+struct AssetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetView()
+    }
+}
