c589cfb1a9323df73cda55a86f29acdd562e7793
finleykim <g_kimmm@naver.com>
2022-07-18 10:34:43 +0900

    [BUILD] gitignore - API키 숨김

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -9,24 +9,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
     @IBOutlet weak var minTempLabel: UILabel!
-    
     @IBOutlet weak var weatherStackView: UIStackView!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
     @IBAction func tapFetchWeatherButton(_ sender: Any) {
         //버튼을 누르면 도시이름을 getCurrentWeather메서드의 파라미터에 전달해서 현재도시의 날씨정보를 가져오도록
         if let cityName = self.cityNameTextField.text { //텍스트필드의 텍스트 가져온다
             self.getCurrentWeather(cityName: cityName)
             self.view.endEditing(true)
         }
-        
-        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
     func configureView(weatherInformation: WeatherInformation){
@@ -49,7 +42,8 @@ class ViewController: UIViewController {
     
     
     func getCurrentWeather(cityName: String){
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=5f2fcea356ec7f45c4f5f2d80f38c01a") else { return }
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else { return }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(apiKey)") else { return }
         let session = URLSession(configuration: .default)
         //서버로 데이터 요청하고 응답받도록
         session.dataTask(with: url) { [weak self] data, response, error in
